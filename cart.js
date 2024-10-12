e35643c81b34b706ccf0a004f5c898e073e86afd
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-13 01:22:28 +0600

    add cart

--- cart.js
+++ cart.js
@@ -5,8 +5,16 @@ const addProduct = () => {
     const product =productFild.value;
     const quentaty =productQutntaty.value;
 
-    productFild.value =' ';
-    productQutntaty.value=' ';
+    productFild.value ='';
+    productQutntaty.value='';
 
-    console.log(product,quentaty)
+    console.log(product,quentaty);
+    displayProduct(product,quentaty)
+}
+
+const displayProduct =(product,quentaty) =>{
+    const productContatiner = document.getElementById('item-contatiner');
+    const p =document.createElement('p');
+    p.innerText =`${product} : ${quentaty}`;
+    productContatiner.appendChild(p)
 }
