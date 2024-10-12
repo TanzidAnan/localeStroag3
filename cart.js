1667ede8102ea6de2b70242d3d3d8d0495b37a78
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-13 01:02:58 +0600

    input value

--- cart.js
+++ cart.js
@@ -1,3 +1,12 @@
 const addProduct = () => {
-    console.log(5454)
+    const productFild =document.getElementById('product-name');
+    const productQutntaty =document.getElementById('product-quentaty');
+
+    const product =productFild.value;
+    const quentaty =productQutntaty.value;
+
+    productFild.value =' ';
+    productQutntaty.value=' ';
+
+    console.log(product,quentaty)
 }
